54d8d778e53980d1ad27a22e3e44d274d33e0e53
Joseph Ogbu <fallyfox@gmail.com>
2023-08-22 12:54:33 +0100

    generated a secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ const authOptions = {
             clientId:process.env.GOOGLE_CLIENT_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
         })
-    ]
+    ],
 }
 
 export default NextAuth(authOptions);
