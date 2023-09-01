cfe86edd4396a92c6164712ab467c67e0ba27aac
Joseph Ogbu <fallyfox@gmail.com>
2023-09-01 17:59:10 +0100

    add image to to post

--- components/WritePost.js
+++ components/WritePost.js
@@ -2,28 +2,52 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import { Button,TextField } from '@mui/material';
-import { db } from '@/settings/firebase.setting';
-import { collection,addDoc } from 'firebase/firestore';
+import { db,storage } from '@/settings/firebase.setting';
+import { collection,addDoc,updateDoc,doc } from 'firebase/firestore';
 import { cdnImages } from '@/assets/demo_cdn_images';
 import { rangeOfRandNums } from '@/assets/range-of-rand-nums';
+import { ref,uploadString,getDownloadURL } from 'firebase/storage';
 
 export default function WritePost() {
     const {data:session} = useSession();
     const [formInput,setFormInput] = useState('');
+    const [selectedFile,setSelectedFile] = useState(null);
+
+    //get image file and convert to base64 string
+    const imageToPost = (e) => {
+        const reader = new FileReader();
+
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+
+        reader.onload = (readEvent) => {
+            setSelectedFile(readEvent.target.result);
+        }
+    }
 
     // create post to firestore
     const handleCreatePost = async () => {
-        await addDoc(collection(db,'posts'),{
+        const docRes = await addDoc(collection(db,'posts'),{
             body:formInput,
             author:session.user.email,
             postedAt:new Date().getTime(),
             imageUrl:cdnImages[rangeOfRandNums(0,cdnImages.length)]
-        })
-        .then(() => {
+        });
+
+        const imageRef = ref(storage,`posts/${docRes.id}/image`);
+
+        await uploadString(imageRef,selectedFile,'data_url')
+        .then(async () => {
+            const imgUrl = await getDownloadURL(imageRef);
+            updateDoc(doc(db,'posts',docRes.id),{
+                imageUrl:imgUrl,
+            });
+
             setFormInput('');
             alert('Your post was published');
         })
-        .catch(error => console.error(error))
+        .catch((e) => console.error(e))
     }
 
     return (
@@ -45,10 +69,18 @@ export default function WritePost() {
                     onChange={(text) => setFormInput(text.target.value)}/>
 
                     {formInput.length > 0
-                    ? <Button 
-                    variant='outlined'
-                    className='block w-[100px]'
-                    onClick={handleCreatePost}>Post</Button>
+                    ? 
+                    <div className='flex flex-col gap-4'>
+                        <input 
+                        type="file" 
+                        accept='image/*'
+                        onChange={imageToPost}/>
+
+                        <Button 
+                        variant='outlined'
+                        className='block w-[100px]'
+                        onClick={handleCreatePost}>Post</Button>
+                    </div>
                     : null}
                 </div>
             </div>
--- settings/firebase.setting.js
+++ settings/firebase.setting.js
@@ -3,6 +3,7 @@
 import { initializeApp,getApp,getApps } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,5 +20,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // const app = initializeApp(firebaseConfig);
 const authentication = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { authentication,db }
+export { authentication,db,storage }
