f5793bfae1e6827dc71ce89a4fb57fc00860250e
Joseph Ogbu <fallyfox@gmail.com>
2023-09-01 13:46:33 +0100

    severside auth and session management

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { cert } from "firebase-admin/app";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 
 export const authOptions = {
     providers:[
@@ -8,6 +10,13 @@ export const authOptions = {
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    adapter: FirestoreAdapter({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+        }),
+    }),
     callbacks:{
         async session({session,user}) {
             // session.tok = '67321774ygrgg42';
